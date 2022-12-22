0d9795aaaa50b550097826d9e111fa51ec3cb840
NikitaZar <webbizon@ya.ru>
2022-12-22 14:31:00 +0400

    init commit

---
+++ app/src/main/java/ru/nikitazar/binlistnet/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.nikitazar.binlistnet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.nikitazar.binlistnet.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
